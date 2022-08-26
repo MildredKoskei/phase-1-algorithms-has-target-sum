c7b54fa7506b876642177b33aa437e117eaffac9
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-25 21:36:52 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 function hasTargetSum(array, target) {
+
+   // this is a two sum function which should sum up to the target indicated
+   //in the function
   // Write your algorithm here
-}
+  let sums = [];
+  // check each element in the array
+  for(let i = 0;i<array.length; i++){
+    //this checks each other element in the given array
+    for(let j = i+1; j<array.length; j++){
+      //determines if these two elements sum up to the target
+      if(array[i] + array[j]===target)return true;
+        
+      } 
+      
+    } 
+        //sums.push([array[i], array[j]]);
+     return false; 
+      }
+    
+  
+
+  //returns all pairs of integers trhat sum to target
+
+hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+hasTargetSum([22, 19, 4, 6, 30], 25);
+hasTargetSum([4], 4);
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -29,6 +54,13 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4,3,1], 10));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
